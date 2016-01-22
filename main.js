dad9b866cd11d0e3bce9ff758b571d28f49c593c
Raheela Khan <raheelak20@gmail.com>
2016-01-22 00:57:37 -0800

    done

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
 (function() {
-  
+
   function displayData(data) {
     var html = "";
     var count = 0;
-
+    var saveForHover = [];
     for (var prop in data) {
       if (count % 5 === 0 ) {
         html += "</div>";
         html += "<div class='row'>";
       }
 
-      html += "<div class='col-lg-2 col-md-2 col-xs-2 display'>";
+      html += "<div class='col-lg-2 col-md-2 col-xs-12 display'>";
       html += "<a href='"+data[prop].link+"'>";
       html += "<img class='img-responsive' src='"+data[prop].author.picture+"'>";
-      html += "<h5>"+data[prop].headline+"</h5>";
-      html += "<p>By: " + data[prop].author.username + "</p>";
-      html += "</a>";
-      html += "<p class='bottom'>" + data[prop].rank + "</p>";
-      html+= "<p> Posted on:" + new Date(1453286131735).toDateString() + "</p>";
+      html += (data[prop].headline.length > 20) ? "<p id='headline' class='headline_"+count+"'>" + data[prop].headline.slice(0,20) + "...</p></a>" : "<p id='headline'class='headline_'"+count+"'>"+ data[prop].headline+"</p></a>"      ;
+      saveForHover.push(data[prop].headline); 
+      html += "<p>By: " + "<a href='"+ data[prop].link+"'>" +  data[prop].author.username + "</a></p>";
+      html += "<p class='bottom fa  fa-heart'> " + data[prop].rank + "</p>";
+      html+= "<p class='bottom'> Posted on:" + new Date(1453286131735).toDateString() + "</p>";
       html += "</div>";
-      console.log(data[prop]);
       count +=1;
     }
     $(".main-row").append(html);
